cabb95f0e177e23b77200c780f2fc40291996ef1
VasilyKolpakov <vasliykolpakov@gmail.com>
2014-03-14 19:40:43 +0400

    hbase 0.96 compatibility

--- src/main/scala/org/kiji/testing/fakehtable/FakeHConnection.scala
+++ src/main/scala/org/kiji/testing/fakehtable/FakeHConnection.scala
@@ -19,14 +19,16 @@
 
 package org.kiji.testing.fakehtable
 
-import org.apache.hadoop.hbase.client.{ServerCallable, Row, HConnection}
+import org.apache.hadoop.hbase.client._
 import java.util
-import org.apache.hadoop.hbase.{HRegionLocation, HServerAddress, HRegionInfo, HTableDescriptor}
-import org.apache.hadoop.hbase.ipc.{HMasterInterface, HRegionInterface, CoprocessorProtocol}
+import org.apache.hadoop.hbase._
 import java.util.concurrent.ExecutorService
-import org.apache.hadoop.hbase.client.coprocessor.Batch.{Callback, Call}
+import org.apache.hadoop.hbase.client.coprocessor.Batch.Callback
 import org.apache.hadoop.hbase.zookeeper.ZooKeeperWatcher
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.protobuf.generated.MasterProtos
+import org.apache.hadoop.hbase.protobuf.generated.AdminProtos.AdminService
+import org.apache.hadoop.hbase.protobuf.generated.ClientProtos.ClientService
 
 /**
  * A fake implementation of HConnection, useful only for support FakeHTable's usage in pools.
@@ -47,14 +49,8 @@ class FakeHConnection extends HConnection {
 
   def clearCaches(sn: String) = throw new UnsupportedOperationException
 
-  def prewarmRegionCache(tableName: Array[Byte], regions: util.Map[HRegionInfo, HServerAddress]) =
-     throw new UnsupportedOperationException
-
   def setRegionCachePrefetch(tableName: Array[Byte], enable: Boolean) =
      throw new UnsupportedOperationException
-
-  def processExecs[T <: CoprocessorProtocol, R](protocol: Class[T], rows: util.List[Array[Byte]], tableName: Array[Byte], pool: ExecutorService, call: Call[T, R], callback: Callback[R]) =
-      throw new UnsupportedOperationException
 
   def processBatchCallback[R](list: util.List[_ <: Row], tableName: Array[Byte], pool: ExecutorService, results: Array[AnyRef], callback: Callback[R]) =
       throw new UnsupportedOperationException
@@ -70,7 +66,7 @@ class FakeHConnection extends HConnection {
 
   def getZooKeeperWatcher: ZooKeeperWatcher = throw new UnsupportedOperationException
 
-  def getMaster: HMasterInterface = throw new UnsupportedOperationException
+  def getMaster: MasterProtos.MasterService.BlockingInterface = throw new UnsupportedOperationException
 
   def isMasterRunning: Boolean = throw new UnsupportedOperationException
 
@@ -97,27 +93,8 @@ class FakeHConnection extends HConnection {
   def locateRegions(tableName: Array[Byte]): util.List[HRegionLocation] =
       throw new UnsupportedOperationException
 
-  def getHRegionConnection(regionServer: HServerAddress): HRegionInterface =
-      throw new UnsupportedOperationException
-
-  def getHRegionConnection(hostname: String, port: Int): HRegionInterface =
-      throw new UnsupportedOperationException
-
-  def getHRegionConnection(regionServer: HServerAddress, getMaster: Boolean): HRegionInterface =
-     throw new UnsupportedOperationException
-
-  def getHRegionConnection(hostname: String, port: Int, getMaster: Boolean): HRegionInterface =
-     throw new UnsupportedOperationException
-
   def getRegionLocation(tableName: Array[Byte], row: Array[Byte], reload: Boolean): HRegionLocation =
       throw new UnsupportedOperationException
-
-  def getRegionServerWithRetries[T](callable: ServerCallable[T]): T =
-      throw new UnsupportedOperationException
-
-  def getRegionServerWithoutRetries[T](callable: ServerCallable[T]): T =
-      throw new UnsupportedOperationException
-
 
   def getRegionCachePrefetch(tableName: Array[Byte]): Boolean =
       throw new UnsupportedOperationException
@@ -128,4 +105,74 @@ class FakeHConnection extends HConnection {
       throw new UnsupportedOperationException
 
   def isAborted: Boolean = false
+
+  override def isDeadServer(serverName: ServerName): Boolean = ???
+
+  override def getKeepAliveMasterService = ???
+
+  override def clearCaches(sn: ServerName): Unit = ???
+
+  override def getHTableDescriptorsByTableName(tableNames: util.List[TableName]): Array[HTableDescriptor] = ???
+
+  override def getRegionCachePrefetch(tableName: TableName): Boolean = ???
+
+  override def setRegionCachePrefetch(tableName: TableName, enable: Boolean): Unit = ???
+
+  override def processBatchCallback[R](list: util.List[_ <: Row], tableName: TableName, pool: ExecutorService, results: Array[AnyRef], callback: Callback[R]): Unit = ???
+
+  override def processBatch(actions: util.List[_ <: Row], tableName: TableName, pool: ExecutorService, results: Array[AnyRef]): Unit = ???
+
+  override def getRegionLocation(tableName: TableName, row: Array[Byte], reload: Boolean): HRegionLocation = ???
+
+  override def getAdmin(serverName: ServerName, getMaster: Boolean): AdminService.BlockingInterface = ???
+
+  override def getClient(serverName: ServerName): ClientService.BlockingInterface = ???
+
+  override def getAdmin(serverName: ServerName): AdminService.BlockingInterface = ???
+
+  override def locateRegions(tableName: Array[Byte], useCache: Boolean, offlined: Boolean): util.List[HRegionLocation] = ???
+
+  override def locateRegions(tableName: TableName, useCache: Boolean, offlined: Boolean): util.List[HRegionLocation] = ???
+
+  override def locateRegions(tableName: TableName): util.List[HRegionLocation] = ???
+
+  override def updateCachedLocations(tableName: Array[Byte], rowkey: Array[Byte], exception: scala.Any, source: HRegionLocation): Unit = ???
+
+  override def updateCachedLocations(tableName: TableName, rowkey: Array[Byte], exception: scala.Any, source: HRegionLocation): Unit = ???
+
+  override def relocateRegion(tableName: TableName, row: Array[Byte]): HRegionLocation = ???
+
+  override def deleteCachedRegionLocation(location: HRegionLocation): Unit = ???
+
+  override def clearRegionCache(tableName: TableName): Unit = ???
+
+  override def locateRegion(tableName: TableName, row: Array[Byte]): HRegionLocation = ???
+
+  override def getHTableDescriptor(tableName: TableName): HTableDescriptor = ???
+
+  override def listTableNames(): Array[TableName] = ???
+
+  override def getTableNames: Array[String] = ???
+
+  override def isTableAvailable(tableName: Array[Byte], splitKeys: Array[Array[Byte]]): Boolean = ???
+
+  override def isTableAvailable(tableName: TableName, splitKeys: Array[Array[Byte]]): Boolean = ???
+
+  override def isTableAvailable(tableName: TableName): Boolean = ???
+
+  override def isTableDisabled(tableName: TableName): Boolean = ???
+
+  override def isTableEnabled(tableName: TableName): Boolean = ???
+
+  override def getTable(tableName: TableName, pool: ExecutorService): HTableInterface = ???
+
+  override def getTable(tableName: Array[Byte], pool: ExecutorService): HTableInterface = ???
+
+  override def getTable(tableName: String, pool: ExecutorService): HTableInterface = ???
+
+  override def getTable(tableName: TableName): HTableInterface = ???
+
+  override def getTable(tableName: Array[Byte]): HTableInterface = ???
+
+  override def getTable(tableName: String): HTableInterface = ???
 }
